b902896417dfb30401daa71690b68e1f0a4390b5
5BNeumann <146111161+5BNeumann@users.noreply.github.com>
2023-11-27 15:18:43 +0100

    Update trait.py

--- trait.py
+++ trait.py
@@ -3,8 +3,9 @@
 # x2, y2 : coordonn´ees de la fin du trait
 from turtle import *
 
-def trait(x1,y1,x2,y2) :
+def trait(x1,y1,x2,y2,w) :
     up()
     goto(x1,y1)
+    width(w)
     down()
     goto(x2,y2)
