ff8737a886d05d687679e8ffaa17cee5d6a1fec0
Pierro999 <96500525+Pierro999@users.noreply.github.com>
2023-12-04 14:33:30 +0100

    facade.py

---
+++ facade.py
@@ -0,0 +1,13 @@
+# Fonction facade(x, y sol, couleur, niveau) :
+# x : abscisse du centre de la facade
+# y sol : ordonn´ee du sol du la rue
+# couleur : couleur de la fa¸cade
+# niveau : num du niveau (0 pour les rdc, ...)
+from turtle import *
+from rectangle import rectangle
+
+def facade(x, y_sol, couleur, niveau) :
+    fillcolor(couleur)
+    begin_fill()
+    rectangle(x,y_sol+60*niveau,140,60)
+    end_fill()
