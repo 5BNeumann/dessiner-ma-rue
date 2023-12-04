d85bb9e107bc938f8c3a96687ba0ff5ce6def574
5BNeumann <146111161+5BNeumann@users.noreply.github.com>
2023-12-04 15:00:56 +0100

    Update fenetre_b.py

--- fenetre_b.py
+++ fenetre_b.py
@@ -6,17 +6,17 @@ from turtle import *
 from trait import *
 from rectangle import *
 
-def fenetre_b(x, y_sol, niveau):
+def fenetre_b(x, y):
     colormode(255)
     fillcolor(196, 214, 215)
     begin_fill()
-    rectangle(x,y_sol*niveau,30,50)
+    rectangle(x,y,30,50)
     end_fill()
-    trait(x-20,y_sol*niveau,x+20,y_sol*niveau,2)
-    trait(x-20,y_sol*niveau+30,x+20,y_sol*niveau+30,2)
+    trait(x-20,y,x+20,y,2)
+    trait(x-20,y+30,x+20,y+30,2)
     xa = x-20
     for i in range(9):
-        trait(xa,y_sol*niveau,xa,y_sol*niveau+30,2)
+        trait(xa,y,xa,y+30,2)
         xa += 5
 if __main__ :
     fenetre_b(0,0,0)
